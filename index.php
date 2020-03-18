9dbe7cf3a828c6a96c68ddd807e5bef80c9fec72
Chris <chunt661@mtroyal.ca>
2020-03-18 12:57:27 -0600

    Added index.php for testing on host server

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>COMP3512-ASG2</title>
+    </head>
+    <body>
+        <h1>Assignment 2</h1>
+        <p>Testing PHP</p>
+    </body>
+</html>
