c5bd78586980171f3dd7ad8e9f76b5f2906e1141
Chris <chunt661@mtroyal.ca>
2020-03-25 12:42:07 -0600

    Fixed fetch type when requesting a single movie. NOTE: May need to move fetch to the include file in the future

--- api-movies-brief.php
+++ api-movies-brief.php
@@ -8,12 +8,12 @@ header("Access-Control-Allow-Origin: *");
 $connection = getConnection();
 
 if (isset($_GET["id"])) {
-    $data = getSingleMovieBrief($connection, $_GET["id"]);
+    $data = getSingleMovieBrief($connection, $_GET["id"])->fetch(PDO::FETCH_ASSOC);
 } else {
-    $data = getAllMovies($connection);
+    $data = getAllMovies($connection)->fetchAll(PDO::FETCH_ASSOC);
 }
 
-echo json_encode($data->fetchAll(PDO::FETCH_ASSOC), JSON_NUMERIC_CHECK);
+echo json_encode($data, JSON_NUMERIC_CHECK);
 
 $connection = null;
 
