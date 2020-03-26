1e834e7d9206795782d51933afdc294f5726161f
Chris <chunt661@mtroyal.ca>
2020-03-26 16:37:42 -0600

    Fixed function name for single movie request

--- api-movies-brief.php
+++ api-movies-brief.php
@@ -8,7 +8,7 @@ header("Access-Control-Allow-Origin: *");
 $connection = getConnection();
 
 if (isset($_GET["id"])) {
-    $data = getSingleMovieBrief($connection, $_GET["id"])->fetch(PDO::FETCH_ASSOC);
+    $data = getSingleMovie($connection, $_GET["id"])->fetch(PDO::FETCH_ASSOC);
 } else {
     $data = getAllMovies($connection)->fetchAll(PDO::FETCH_ASSOC);
 }
