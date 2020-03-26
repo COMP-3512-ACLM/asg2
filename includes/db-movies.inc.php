72189bc5297f937c3f138c66b45bca210414506f
Chris <chunt661@mtroyal.ca>
2020-03-26 09:49:12 -0600

    Removed an unnecessary query function. Basically, I fixed my previous fix

--- includes/db-movies.inc.php
+++ includes/db-movies.inc.php
@@ -2,29 +2,20 @@
 
 include "db-common.inc.php";
 
-define("DBFIELDSBRIEF", "id, tmdb_id, imdb_id, release_date, title, runtime, revenue, tagline, poster_path, vote_average, vote_count, popularity, overview");
-
-// Returns brief, important information on all movies.
+// Returns brief information on all movies.
 function getAllMovies($connection) {
     try {
-        $sql = "SELECT " . DBFIELDSBRIEF . " FROM movie";
+        // The fields to be selected in the query. This is to make the sql variable more readable and to make it easier to modify the query fields
+        $fields = "id, tmdb_id, imdb_id, release_date, title, runtime, revenue, tagline, poster_path, vote_average, vote_count, popularity, overview";
+        
+        $sql = "SELECT $fields FROM movie";
         return runQuery($connection, $sql, null);
     } catch (PDOException $e) {
         die($e->getMessage());
     }
 }
 
-// Returns brief, important information on a single movie.
-function getSingleMovieBrief($connection, $id) {
-    try {
-        $sql = "SELECT " . DBFIELDSBRIEF . " FROM movie WHERE id=?";
-        return runQuery($connection, $sql, $id);
-    } catch (PDOException $e) {
-        die($e->getMessage());
-    }
-}
-
-// Returns ALL information on a single movie.
+// Returns all information on a single movie.
 function getSingleMovie($connection, $id) {
     try {
         $sql = "SELECT * FROM movie WHERE id=?";
