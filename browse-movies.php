4517d5ef496f4d5830c74ef2f3c9c44231a70d0e
Chris <chunt661@mtroyal.ca>
2020-04-01 20:56:36 -0600

    Added simple styling

--- browse-movies.php
+++ browse-movies.php
@@ -4,11 +4,13 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="style/reset.css" />
+        <link rel="stylesheet" href="style/style.css" />
+        <script src="script/browse.js"></script>
     </head>
     <body>
         <!-- TODO: create header via PHP -->
         <!--Filters-->
-        <div>
+        <div id="filters" class="container">
             <h1>Movie Filters</h1>
             <form>
                 <!--Title-->
@@ -20,39 +22,51 @@
                 <fieldset>
                     <legend>Year</legend>
                     <!--Before-->
-                    <input name="year" type="radio" />
-                    <label>Before</label>
-                    <input type="text" />
+                    <div>
+                        <input name="year" type="radio" />
+                        <label>Before</label>
+                        <input type="text" />
+                    </div>
                     <!--After-->
-                    <input name="year" type="radio" />
-                    <label>After</label>
-                    <input type="text" />
+                    <div>
+                        <input name="year" type="radio" />
+                        <label>After</label>
+                        <input type="text" />
+                    </div>
                     <!--Between-->
-                    <input name="year" type="radio" />
-                    <label>Between</label>
-                    <input type="text" />
-                    <input type="text" />
+                    <div>
+                        <input name="year" type="radio" />
+                        <label>Between</label>
+                        <input type="text" />
+                        <input type="text" />
+                    </div>
                 </fieldset>
                 <!--Rating-->
                 <fieldset>
                     <legend>Rating</legend>
                     <!--Below-->
-                    <input name="rating" type="radio" />
-                    <label>Below</label>
-                    <input name="below" type="range" min="0" max="10" value="5" />
-                    <output for="below"></output>
+                    <div>
+                        <input name="rating" type="radio" />
+                        <label>Below</label>
+                        <input name="below" type="range" min="0" max="10" value="5" />
+                        <output for="below">1</output>
+                    </div>
                     <!--Above-->
-                    <input name="rating" type="radio" />
-                    <label>Above</label>
-                    <input name="above" type="range" min="0" max="10" value="5" />
-                    <output for="above"></output>
+                    <div>
+                        <input name="rating" type="radio" />
+                        <label>Above</label>
+                        <input name="above" type="range" min="0" max="10" value="5" />
+                        <output for="above">3</output>
+                    </div>
                     <!--Between-->
-                    <input name="rating" type="radio" />
-                    <label>Between</label>
-                    <input name="between-lower" type="range" min="0" max="10" value="5" />
-                    <output for="between-lower"></output>
-                    <input name="between-upper" type="range" min="0" max="10" value="5" />
-                    <output for="between-upper"></output>
+                    <div>
+                        <input name="rating" type="radio" />
+                        <label>Between</label>
+                        <input name="between-lower" type="range" min="0" max="10" value="5" />
+                        <output for="between-lower">5</output>
+                        <input name="between-upper" type="range" min="0" max="10" value="5" />
+                        <output for="between-upper">10</output>
+                    </div>
                 </fieldset>
                 <button>Filter</button>
                 <button>Clear</button>
