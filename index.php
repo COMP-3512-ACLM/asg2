f5d34c07c292e7f98429e7d0c93a8ba937b8d4f3
Chris <chunt661@mtroyal.ca>
2020-04-08 13:48:33 -0600

    Added basic logged out homepage

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
+<?php
+
+include "includes/helpers.inc.php";
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>COMP3512-ASG2</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="style/reset.css" />
+        <link rel="stylesheet" href="style/style.css" />
+        <script src="script/header.js"></script>
     </head>
     <body>
-        <h1>Assignment 2</h1>
-        <p>Testing PHP</p>
+        <?php
+        $loggedin = false; // TODO: temporary, remove this
+
+        if ($loggedin) {
+        ?>
+        <!-- BEGIN Logged in version -->
+        <?php outputHeader() ?>
+        
+        <!-- END Logged in version -->
+        <?php } else { ?>
+        <!-- BEGIN Logged out version -->
+        <div class="hero-img"></div>
+        <div class="landing">
+            <h1>Search Movies</h1>
+            <form action="browse-movies.php">
+                <input name="search" type="text" placeholder="Search movies" />
+                <button class="icon">🔍</button>
+            </form>
+            <div class="button-container">
+                <a class="login">Login</a>
+                <a class="login important">Sign Up</a>
+            </div>
+        </div>
+        <!-- END Logged out version -->
+        <?php
+        }
+        ?>
     </body>
 </html>
