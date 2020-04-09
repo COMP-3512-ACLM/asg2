05e4fd3a0d29d02d2fcce35f7e06f8c37f49cee3
Chris <chunt661@mtroyal.ca>
2020-04-09 12:25:01 -0600

    Added simple layout styling for login and sign up

--- login.php
+++ login.php
@@ -6,23 +6,24 @@ require_once 'includes/db-common.inc.php';
 
 ?>
 <!DOCTYPE html>
-<html lang=en>
-<head>
-</head>
-<body >
-    
-<main>
-    <h1>Log In</h1>
-    <p><?php echo $loginError; ?></p>
-    <form method="post" action=""> 
-        <span>Email</span><input type="email" name="email">
-        <br>
-        <span>Password</span><input type="password" name="password">
-        
-        <input type="submit" name="loginButton">
-    </form>
-
-    
-</main>    
-</body>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="style/reset.css" />
+        <link rel="stylesheet" href="style/style.css" />
+        <script src="script/browse.js"></script>
+    </head>
+    <body>
+        <main>
+            <h1>Log In</h1>
+            <p><?php echo $loginError; ?></p>
+            <form method="post" action="" class="login"> 
+                <span>Email</span><input type="email" name="email">
+                <br>
+                <span>Password</span><input type="password" name="password">
+                <input type="submit" name="loginButton">
+            </form>
+        </main>
+    </body>
 </html>
--- signup.php
+++ signup.php
@@ -4,30 +4,29 @@ require_once 'includes/db-users.inc.php';
 require_once 'includes/db-common.inc.php';
 ?>
 <!DOCTYPE html>
-<html lang=en>
-<head>
-</head>
-<body >
-    
-<main>
-    <h1>Sign Up</h1>
-    
-    <form method="post" action=""> 
-        <p><?php echo $errorMessage; ?></p>
-        <br>
-        <span>First Name</span><input type="text" placeholder="First Name" name="fName" value="<?php displayFirst(); ?>" required> <br>
-        <span>Last Name</span><input type="text" placeholder="Last Name" name="lName" value="<?php displayLast(); ?>" required><br>
-        <span>City</span><input type="text" placeholder="City" name="city" value="<?php displayCity(); ?>"  required> <br>
-        <span>Country</span><input type="text" placeholder="Country" name="country" value="<?php displayCountry(); ?>"  required> <br>
-        <span>Email</span><input type="email" placeholder="email@example.com" name="newEmail" value="<?php displayEmail(); ?>"  required><?php ?><br>
-        <span>Password</span><input type="password" minlength="8" name="newPass" required><br>
-        <span>Confirm Password</span><input type="password" minlength="8" name="confirmPass" required><br>
-        <input type="submit" name="signupSubmit">
-            Click
-    </form>
-    
-    
-    
-</main>    
-</body>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="style/reset.css" />
+        <link rel="stylesheet" href="style/style.css" />
+        <script src="script/browse.js"></script>
+    </head>
+    <body>
+        <main>
+            <h1>Sign Up</h1>
+            <form method="post" action="" class="login"> 
+                <p><?php echo $errorMessage; ?></p>
+                <br>
+                <span>First Name</span><input type="text" placeholder="First Name" name="fName" value="<?php displayFirst(); ?>" required> <br>
+                <span>Last Name</span><input type="text" placeholder="Last Name" name="lName" value="<?php displayLast(); ?>" required><br>
+                <span>City</span><input type="text" placeholder="City" name="city" value="<?php displayCity(); ?>"  required> <br>
+                <span>Country</span><input type="text" placeholder="Country" name="country" value="<?php displayCountry(); ?>"  required> <br>
+                <span>Email</span><input type="email" placeholder="email@example.com" name="newEmail" value="<?php displayEmail(); ?>"  required><?php ?><br>
+                <span>Password</span><input type="password" minlength="8" name="newPass" required><br>
+                <span>Confirm Password</span><input type="password" minlength="8" name="confirmPass" required><br>
+                <input type="submit" name="signupSubmit">
+            </form>
+        </main>
+    </body>
 </html>
